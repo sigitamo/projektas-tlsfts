81ba2cee3a24f085d856cc760348bc5ab6a3a5af
sigitamocevice <sigita.z@gmail.com>
2017-09-07 10:21:13 +0300

    validators

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm, FormGroup } from '@angular/forms';
+import { NgForm, FormGroup, Validators, FormControl } from '@angular/forms';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
@@ -8,7 +8,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-// userForm: FormGroup; - Šis priklausys Validators
+
 results: string[];
  
 
@@ -20,12 +20,24 @@ results: string[];
 
   onRegistered(form: NgForm) {
     console.log('SUBMIT');
-    const username = form.value.username;
-    const firstname = form.value.firstname;
-    const lastname = form.value.lastname;
-    const phonenumber = form.value.phonenumber;
-    const password = form.value.password;
-    console.log(form);
+    const formUser = new FormGroup({
+      username: new FormControl(form.value.username, Validators.minLength(3)),
+      firstname: new FormControl (form.value.firstname, Validators.minLength(3)),
+      lastname: new FormControl(form.value.lastname, Validators.minLength(3)),
+      phonenumber: new FormControl(form.value.phonenumber, Validators.minLength(8)),
+      password: new FormControl(form.value.password, Validators.minLength(6)),
+      passwordConfirm: new FormControl(form.value.passwordConfirm, Validators.minLength(6))
+    }, passwordMatchValidator);
+    function passwordMatchValidator(g: FormGroup) {
+      return g.get('password').value === g.get('passwordConfirm').value ? null : {'mismatch': true};
+  }
+
+    // const username = form.value.username;
+    // const firstname = form.value.firstname;
+    // const lastname = form.value.lastname;
+    // const phonenumber = form.value.phonenumber;
+    // const password = form.value.password;
+    console.log(formUser);
     this.httpClient.post('', null, {
       params: new HttpParams().set('input', '123'),
     })
@@ -39,8 +51,5 @@ results: string[];
   
     // form.reset();
   }
-// čia reikia atlikti validators
-  // onRegisteredUser() {
-  //   <FormArray>this.userForm.get('users')
-  // }
+ 
 }
